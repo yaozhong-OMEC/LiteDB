3b1701f00666972a03db276740b8bde8e80a3af7
Jens Schadron <jensschadron@hotmail.com>
2019-08-27 22:13:23 +0200

    Cleaned up AppDelegate

--- LiteDB.Studio.Mac/AppDelegate.cs
+++ LiteDB.Studio.Mac/AppDelegate.cs
@@ -1,32 +1,11 @@
-﻿using System.Linq;
-using AppKit;
-using Foundation;
+﻿using Foundation;
 using LiteDB.Studio.Core;
-using MvvmCross;
 using MvvmCross.Platforms.Mac.Core;
-using MvvmCross.ViewModels;
 
 namespace LiteDB.Studio.Mac
 {
     [Register(nameof(AppDelegate))]
     public class AppDelegate : MvxApplicationDelegate<Setup, App>
     {
-        /*public AppDelegate() : base()
-        {
-            /*caret#1#
-        }
-
-        public override void DidFinishLaunching(NSNotification notification)
-        {
-            // Insert code here to initialize your application
-            base.DidFinishLaunching(notification);
-        }
-
-        public override void WillTerminate(NSNotification notification)
-        {
-            // Insert code here to tear down your application
-            base.WillTerminate(notification);
-        }*/
-         
     }
 }
